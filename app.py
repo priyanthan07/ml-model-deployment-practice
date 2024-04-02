74642878b511efed3b97d5796c689a93235e88de
LAPTOP-JNT30UUH\Salus X Labs <govindarajpriyanthan@gmail.com>
2024-04-02 14:05:40 +0530

    removed debug option

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def predict_datapoint():
     
 
 if __name__ == "__main__":
-    # app.run('0.0.0.0')
-    app.run('0.0.0.0', debug= True)
+    app.run('0.0.0.0')
+    # app.run('0.0.0.0', debug= True)
 
 
