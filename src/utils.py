9952046b6ca6abe36b40c035dbe3a4cafd37e13a
LAPTOP-JNT30UUH\Salus X Labs <govindarajpriyanthan@gmail.com>
2024-04-06 20:39:24 +0530

    deleted the app.py

--- src/utils.py
+++ src/utils.py
@@ -43,11 +43,8 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
             #model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
-
             y_test_pred = model.predict(X_test)
-
             train_model_score = r2_score(y_train, y_train_pred)
-
             test_model_score = r2_score(y_test, y_test_pred)
 
             report[list(models.keys())[i]] = test_model_score
@@ -56,7 +53,6 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
 
     except Exception as e:
         raise CustomException(e, sys) 
-    
 
 
 # function used to load the saved object
