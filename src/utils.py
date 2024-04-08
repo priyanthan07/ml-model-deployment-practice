c136c6d0336194da78ecb51e8cef8b65f7570d80
LAPTOP-JNT30UUH\Salus X Labs <govindarajpriyanthan@gmail.com>
2024-04-08 21:05:21 +0530

    removed some folders

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,6 @@ import sys
 import numpy as np
 import pandas as pd
 import dill              # used to create a pickle file
-import pickle
 
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
@@ -19,7 +18,7 @@ def save_object(file_path, obj):
         os.makedirs(dir_path, exist_ok= True)
 
         with open(file_path, 'wb') as file_obj:
-            pickle.dump(obj, file_obj)
+            dill.dump(obj, file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
@@ -59,7 +58,7 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
 def load_object(file_path):
     try:
         with open(file_path, 'rb') as file_obj:
-            return pickle.load(file_obj)
+            return dill.load(file_obj)
         
     except Exception as e:
         raise CustomException(e, sys)
